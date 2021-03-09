68a059c1d704ab2e0e37f06ff0d811c77805580d
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2021-03-08 20:05:48 -0500

    set up deploy to heroku

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(_dirname + '/dist/the-generic-app'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(_dirname+'/dist/the-generic-app/index.html'));});
+    res.sendFile(path.join(_dirname + '/dist/the-generic-app/index.html'));});
 app.listen(process.env.PORT || 8080);
